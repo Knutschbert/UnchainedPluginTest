ddb5ca8efc09268e3d61ecc34246fe678bb4fead
Atom Roese <40679658+ARoese@users.noreply.github.com>
2023-07-19 05:50:49 -0400

    Set backend addresses, add MOTD hook and logging
    Changes the address of the backend to servers.polehammer.net, adds a hook to the getMotd function, and adds logging for debug builds

--- C2BrowserPlugin/main.cpp
+++ C2BrowserPlugin/main.cpp
@@ -1,5 +1,9 @@
 #include <Windows.h>
 #include <MinHook/include/MinHook.h>
+#include <iostream>
+
+//#define TARGET_API_ROOT L"localhost"
+#define TARGET_API_ROOT L"servers.polehammer.net"
 
 struct FString {
 	FString(const wchar_t* str) {
@@ -17,37 +21,79 @@ struct GCGObj {
 	FString url_base;
 };
 
+void log(const char* str) {
+#ifndef _DEBUG
+	return;
+#endif
+	std::cout << str << std::endl;
+
+}
+
+int logWideString(wchar_t* str) {
+#ifndef _DEBUG
+	return 0;
+#endif
+	int i = 0;
+	while (*(char*)str != 0) {
+		std::cout << *(char*)str;
+		str+=2;
+		i++;
+	}
+	std::cout << std::endl;
+	return i;
+}
+
 typedef void* (*GetCurrentGames_t)(GCGObj*, void*, void*, void*);
 GetCurrentGames_t o_GetCurrentGames;
 
 typedef FString* (*ConcatUrl_t)(FString*, FString*);
 ConcatUrl_t o_ConcatUrl;
 
+typedef wchar_t** (*GetMotd_t)(GCGObj* this_ptr, void* a2, void* a3, void* a4);
+GetMotd_t o_GetMotd;
+
+void* hk_GetMotd(GCGObj* this_ptr, void* a2, void* a3, void* a4) {
+	auto old_base = this_ptr->url_base;
+	this_ptr->url_base = FString(L"http://" TARGET_API_ROOT L"/api/tbio");
+	void* res = o_GetMotd(this_ptr, a2, a3, a4);
+
+	this_ptr->url_base = old_base;
+	log("GetMotd returned");
+	return res;
+}
+
 void* hk_GetCurrentGames(GCGObj* this_ptr, void* a2, void* a3, void* a4) {
+	log("GetCurrentGames called");
 	auto old_base = this_ptr->url_base;
 
-	this_ptr->url_base = FString( L"https://google.com" );
+	this_ptr->url_base = FString( L"http://" TARGET_API_ROOT "/api/tbio" );
 	void* res{ o_GetCurrentGames(this_ptr, a2, a3, a4) };
 
 	this_ptr->url_base = old_base;
+	log("GetCurrentGames returned");
 	return res;
 }
 
 FString* hk_ConcatUrl(FString* final_url, FString* url_path) {
 	if (wcscmp(url_path->str, L"/Client/Matchmake") != 0) return o_ConcatUrl(final_url, url_path);
 	
-	const wchar_t* custom_url{ L"https://google.com/Client/Matchmake" };
+	log("ConcatUrl: Substituting matchmaking call");
+
+	const wchar_t* custom_url{ L"http://" TARGET_API_ROOT "/api/playfab/Client/Matchmake" };
 	final_url->letter_count = lstrlenW(custom_url) + 1;
 
 	wcscpy_s(final_url->str, static_cast<size_t>(final_url->letter_count), custom_url);
-
 	return final_url;
 }
 
 unsigned long main_thread(void* lpParameter) {
+	log("BrowserPlugin started!");
 	MH_Initialize();
 
 	unsigned char* module_base{ reinterpret_cast<unsigned char*>(GetModuleHandleA("Chivalry2-Win64-Shipping.exe")) };
+
+	MH_CreateHook(module_base + 0x13da7d0, hk_GetMotd, reinterpret_cast<void**>(&o_GetMotd));
+	MH_EnableHook(module_base + 0x13da7d0);
 
 	MH_CreateHook(module_base + 0x13DA280, hk_GetCurrentGames, reinterpret_cast<void**>(&o_GetCurrentGames));
 	MH_EnableHook(module_base + 0x13DA280);
@@ -60,6 +106,13 @@ unsigned long main_thread(void* lpParameter) {
 }
 
 int __stdcall DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+	#ifdef _DEBUG
+	if (!GetConsoleWindow()) {
+		AllocConsole();
+		FILE* file_pointer{};
+		freopen_s(&file_pointer, "CONOUT$", "w", stdout);
+	}
+	#endif
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH: {
 		HANDLE thread_handle{ CreateThread(NULL, 0, main_thread, hModule, 0, NULL) };
