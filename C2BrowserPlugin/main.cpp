69b05d4a73e83bce6c261511767a36543746e57f
Atom Roese <40679658+ARoese@users.noreply.github.com>
2023-07-21 16:09:53 -0400

    Fix game crash on server join
    Change concatURL hook to SendRequest hook

--- C2BrowserPlugin/main.cpp
+++ C2BrowserPlugin/main.cpp
@@ -86,6 +86,26 @@ FString* hk_ConcatUrl(FString* final_url, FString* url_path) {
 	return final_url;
 }
 
+
+typedef wchar_t** (*SendRequest_t)(GCGObj* this_ptr, FString* a2, FString* a3, FString* a4, FString* a5);
+SendRequest_t o_SendRequest;
+
+
+void* hk_SendRequest(GCGObj* this_ptr, FString* a2, FString* a3, FString* a4, FString* a5) {
+
+	if (a2->letter_count > 0 &&
+		wcscmp(L"https://EBF8D.playfabapi.com/Client/Matchmake?sdk=Chiv2_Version", a2->str) == 0)
+	{
+		FString original = *a2; //save original string and buffer information
+		*a2 = FString(L"http://" TARGET_API_ROOT "/api/playfab/Client/Matchmake"); //overwrite with new string
+		log("hk_SendRequest Client/Matchmake");
+		auto res = o_SendRequest(this_ptr, a2, a3, a4, a5); //run the request as normal with new string
+		*a2 = original; //set everything back to normal and pretend nothing happened
+		return res;
+	}
+	return o_SendRequest(this_ptr, a2, a3, a4, a5);
+}
+
 unsigned long main_thread(void* lpParameter) {
 	log("BrowserPlugin started!");
 	MH_Initialize();
@@ -98,8 +118,11 @@ unsigned long main_thread(void* lpParameter) {
 	MH_CreateHook(module_base + 0x13DA280, hk_GetCurrentGames, reinterpret_cast<void**>(&o_GetCurrentGames));
 	MH_EnableHook(module_base + 0x13DA280);
 
-	MH_CreateHook(module_base + 0x14ACB70, hk_ConcatUrl, reinterpret_cast<void**>(&o_ConcatUrl));
-	MH_EnableHook(module_base + 0x14ACB70);
+	/*MH_CreateHook(module_base + 0x14ACB70, hk_ConcatUrl, reinterpret_cast<void**>(&o_ConcatUrl));
+	MH_EnableHook(module_base + 0x14ACB70);*/
+
+	MH_CreateHook(module_base + 0x14a1250, hk_SendRequest, reinterpret_cast<void**>(&o_SendRequest));
+	MH_EnableHook(module_base + 0x14a1250);
 
 	ExitThread(0);
 	return 0;
