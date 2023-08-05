c4398df8e55e58794ef28f90d66063a91b784c45
Knutschbert <knutschbert@gmail.com>
2023-08-06 01:23:53 +0200

    Squashed commit of the following:
    commit 030b54ed3497c5af68fed06f8ca0770c1e0076d4
    Author: Knutschbert <knutschbert@gmail.com>
    Date:   Fri Jul 21 21:28:06 2023 +0200
    
        Client/Matchmake: Hook SendRequest instead of GetURL. Fixes client crash on join
    
        Added sig scanning lib (https://github.com/HoShiMin/Sig). SendRequest, GetMotd and GetCurrentGames can be found.
    
        Implemented Sigscanning. Merged functions from AssetLoader and Server plugins
    
        Sync local code. JSON import/export working. Add TinyJSON lib

---
+++ C2BrowserPlugin/include/main.h
@@ -0,0 +1,100 @@
+﻿#pragma once
+#include <Sig/Sig.hpp>
+#include "sigs.h"
+
+const char* logo = "_________  .__     .__                .__                    ________            \n \
+\\_   ___ \\ |  |__  |__|___  _______   |  |  _______  ___.__. \\_____  \\       \n \
+/    \\  \\/ |  |  \\ |  |\\  \\/ /\\__  \\  |  |  \\_  __ \\<   |  |  /  ____/  \n \
+\\     \\____|   Y  \\|  | \\   /  / __ \\_|  |__ |  | \\/ \\___  | /       \\   \n \
+ \\______  /|___|  /|__|  \\_/  (____  /|____/ |__|    / ____| \\_______ \\      \n \
+        \\/      \\/                 \\/                \\/              \\/     \n \
+ ____ ___                 .__             .__                     .___           \n \
+|    |   \\  ____    ____  |  |__  _____   |__|  ____    ____    __| _/          \n \
+|    |   / /    \\ _/ ___\\ |  |  \\ \\__  \\  |  | /    \\ _/ __ \\  / __ |     \n \
+|    |  / |   |  \\\\  \\___ |   Y  \\ / __ \\_|  ||   |  \\\\  ___/ / /_/ |     \n \
+|______/  |___|  / \\___  >|___|  /(____  /|__||___|  / \\___  >\\____ |         \n \
+               \\/      \\/      \\/      \\/          \\/      \\/      \\/     \n";
+
+// UE Types
+struct FString {
+	FString(const wchar_t* str) {
+		this->letter_count = lstrlenW(str) + 1;
+		this->max_letters = this->letter_count;
+		this->str = const_cast<wchar_t*>(str);
+	}
+
+	wchar_t* str;
+	int letter_count;
+	int max_letters;
+};
+
+struct GCGObj {
+	FString url_base;
+};
+
+// Helper functions
+
+void log(const char* str) {
+#ifndef _DEBUG
+	return;
+#endif
+	std::cout << str << std::endl;
+
+}
+
+int logWideString(wchar_t* str) {
+#ifndef _DEBUG
+	return 0;
+#endif
+	int i = 0;
+	while (*(char*)str != 0) {
+		std::cout << *(char*)str;
+		str += 2;
+		i++;
+	}
+	std::cout << std::endl;
+	return i;
+}
+
+
+long long FindSignature(HMODULE baseAddr, DWORD size, const char* title, const char* signature)
+{
+	const void* found = nullptr;
+	found = Sig::find(baseAddr, size, signature);
+	long long diff = 0;
+	if (found != nullptr)
+	{
+		diff = (long long)found - (long long)baseAddr;
+#ifdef _DEBUG
+		//std::cout << title << ": 0x" << std::hex << diff << std::endl;
+		printf("?? -> %s : 0x%llx\n", title, diff);
+#endif
+	}
+#ifdef _DEBUG
+	else
+		printf("!! -> %s : nullptr\n", title);
+		//std::cout << title << ": nullptr" << std::endl;
+#endif
+
+		return diff;
+
+}
+// Hook macros
+
+HMODULE baseAddr;
+MODULEINFO moduleInfo;
+
+#define DECL_HOOK(retType, funcType, args)    \
+	typedef retType (*funcType##_t) args;		\
+	funcType##_t o_##funcType;					\
+	retType hk_##funcType args
+
+#define HOOK_ATTACH(moduleBase, funcType) \
+	MH_CreateHook(moduleBase + curBuild.offsets[F_##funcType], hk_##funcType, reinterpret_cast<void**>(&o_##funcType)); \
+	MH_EnableHook(moduleBase + curBuild.offsets[F_##funcType]); 
+
+#define HOOK_FIND_SIG(funcType) \
+	if (curBuild.offsets[F_##funcType] == 0)\
+		curBuild.offsets[F_##funcType] = FindSignature(baseAddr, moduleInfo.SizeOfImage, #funcType, signatures[F_##funcType]); \
+	else printf("-> %s : (conf)\n", #funcType);
+	//long long sig_##funcType = FindSignature(baseAddr, moduleInfo.SizeOfImage, #funcType, signatures[F_##funcType]);
--- C2BrowserPlugin/main.cpp
+++ C2BrowserPlugin/main.cpp
@@ -1,7 +1,14 @@
 #include <Windows.h>
+#include <Psapi.h>
 #include <MinHook/include/MinHook.h>
 #include <iostream>
-
+#include "include/main.h"
+#include "tiny-json/tiny-json.h"
+#include <deque>
+#include <fcntl.h>
+#include <io.h>
+#include <fstream>
+#include <iomanip>
 //#define TARGET_API_ROOT L"localhost"
 #define TARGET_API_ROOT L"servers.polehammer.net"
 #ifdef _DEBUG
@@ -10,51 +17,42 @@
 	#define PROTOCOL L"https"
 #endif
 
-struct FString {
-	FString(const wchar_t* str) {
-		this->letter_count = lstrlenW(str) + 1;
-		this->max_letters = this->letter_count;
-		this->str = const_cast<wchar_t*>(str);
-	}
+#include <cstdint>
+#include <nmmintrin.h> // SSE4.2 intrinsics
 
-	wchar_t* str;
-	int letter_count;
-	int max_letters;
+struct BuildType {
+	uint32_t fileHash = 0;
+	uint32_t buildId = 0;
+	uint32_t offsets[F_MaxFuncType] = {};
+	std::string name = "<unnamed>";
 };
 
-struct GCGObj {
-	FString url_base;
-};
+std::deque<BuildType*> configBuilds;
 
-void log(const char* str) {
-#ifndef _DEBUG
-	return;
-#endif
-	std::cout << str << std::endl;
-
-}
-
-int logWideString(wchar_t* str) {
-#ifndef _DEBUG
-	return 0;
-#endif
-	int i = 0;
-	while (*(char*)str != 0) {
-		std::cout << *(char*)str;
-		str+=2;
-		i++;
+uint32_t calculateCRC32(const std::string& filename) {
+	std::ifstream file(filename, std::ios::binary);
+	if (!file.is_open()) {
+		std::cerr << "Error opening file: " << filename << std::endl;
+		return 0;
 	}
-	std::cout << std::endl;
-	return i;
+
+	uint32_t crc = 0; // Initial value for CRC-32
+
+	char buffer[4096];
+	while (file) {
+		file.read(buffer, sizeof(buffer));
+		std::streamsize bytesRead = file.gcount();
+
+		for (std::streamsize i = 0; i < bytesRead; ++i) {
+			crc = _mm_crc32_u8(crc, buffer[i]);
+		}
+	}
+
+	file.close();
+	return crc ^ 0xFFFFFFFF; // Final XOR value for CRC-32
 }
 
-typedef void* (*GetCurrentGames_t)(GCGObj*, void*, void*, void*);
-GetCurrentGames_t o_GetCurrentGames;
-
-typedef wchar_t** (*GetMotd_t)(GCGObj* this_ptr, void* a2, void* a3, void* a4);
-GetMotd_t o_GetMotd;
-
-void* hk_GetMotd(GCGObj* this_ptr, void* a2, void* a3, void* a4) {
+DECL_HOOK(void*, GetMotd, (GCGObj* this_ptr, void* a2, void* a3, void* a4)) {
 	auto old_base = this_ptr->url_base;
 	this_ptr->url_base = FString( PROTOCOL L"://" TARGET_API_ROOT L"/api/tbio");
 	void* res = o_GetMotd(this_ptr, a2, a3, a4);
@@ -64,7 +62,7 @@ void* hk_GetMotd(GCGObj* this_ptr, void* a2, void* a3, void* a4) {
 	return res;
 }
 
-void* hk_GetCurrentGames(GCGObj* this_ptr, void* a2, void* a3, void* a4) {
+DECL_HOOK(void*, GetCurrentGames, (GCGObj* this_ptr, void* a2, void* a3, void* a4)) {
 	log("GetCurrentGames called");
 	auto old_base = this_ptr->url_base;
 
@@ -76,11 +74,7 @@ void* hk_GetCurrentGames(GCGObj* this_ptr, void* a2, void* a3, void* a4) {
 	return res;
 }
 
-typedef wchar_t** (*SendRequest_t)(GCGObj* this_ptr, FString* a2, FString* a3, FString* a4, FString* a5);
-SendRequest_t o_SendRequest;
-
-
-void* hk_SendRequest(GCGObj* this_ptr, FString* a2, FString* a3, FString* a4, FString* a5) {
+DECL_HOOK(void*, SendRequest, (GCGObj* this_ptr, FString* a2, FString* a3, FString* a4, FString* a5)) {
 
 	if (a2->letter_count > 0 &&
 		wcscmp(L"https://EBF8D.playfabapi.com/Client/Matchmake?sdk=Chiv2_Version", a2->str) == 0)
@@ -95,20 +89,339 @@ void* hk_SendRequest(GCGObj* this_ptr, FString* a2, FString* a3, FString* a4, FS
 	return o_SendRequest(this_ptr, a2, a3, a4, a5);
 }
 
+
+
+// AssetLoaderPlugin
+
+DECL_HOOK(long long, FindFileInPakFiles_1, (void* this_ptr, const wchar_t* Filename, void** OutPakFile, void* OutEntry)) {
+	auto attr{ GetFileAttributesW(Filename) };
+	if (attr != INVALID_FILE_ATTRIBUTES && Filename && wcsstr(Filename, L"../../../")) {
+		if (OutPakFile) OutPakFile = nullptr;
+		return 0;
+	}
+
+	return o_FindFileInPakFiles_1(this_ptr, Filename, OutPakFile, OutEntry);
+}
+
+DECL_HOOK(long long, FindFileInPakFiles_2, (void* this_ptr, const wchar_t* Filename, void** OutPakFile, void* OutEntry)) {
+	auto attr{ GetFileAttributesW(Filename) };
+	if (attr != INVALID_FILE_ATTRIBUTES && Filename && wcsstr(Filename, L"../../../")) {
+		if (OutPakFile) OutPakFile = nullptr;
+		return 0;
+	}
+
+	return o_FindFileInPakFiles_2(this_ptr, Filename, OutPakFile, OutEntry);
+}
+
+DECL_HOOK(long long, IsNonPakFilenameAllowed, (void* this_ptr, void* InFilename)) {
+	return 1;
+}
+
+//FString* __cdecl
+//UUserFeedbackAndBugReportsLibrary::GetGameInfo(FString* __return_storage_ptr__, UWorld* param_1)
+DECL_HOOK(FString*, GetGameInfo, (FString* ret_ptr, void* uWorld))
+{
+	auto val = o_GetGameInfo(ret_ptr, uWorld);
+#ifdef _DEBUG
+	std::wcout << "GetGameInfo: " << *val->str << std::endl;
+#endif
+	return val;
+}
+
+//FViewport* __thiscall FViewport::FViewport(FViewport* this, FViewportClient* param_1)
+struct FViewport_C
+{
+	uint8_t ph[0x20];
+	FString AppVersionString;
+};
+
+// FIXME
+struct BuildInfo
+{
+	BuildInfo() {}
+	BuildInfo(const wchar_t* str, uint32_t id) //: buildStr(str), buildId(id) {}
+	{
+		size_t sz;
+		buildStr = (char*)malloc(128 * MB_CUR_MAX);
+		wcstombs_s(&sz, buildStr, 128 * MB_CUR_MAX, str, 127);
+		//std::cout << "parsed: " << buildStr << std::endl;
+		//wctomb(buildStr, *str);
+		buildId = id;
+	}
+	char * buildStr;
+	uint32_t buildId;
+};
+
+//BuildInfo* curBuildInfo = nullptr;
+BuildType curBuild;
+bool jsonDone = false;
+bool offsetsLoaded = false;
+bool needsSerialization = true;
+
+void serializeBuilds()
+{
+	char buff[2048];
+	char* dest = buff;
+	/*if (offsetsLoaded && curBuild == nullptr)
+	{
+		needsSerialization = false;
+		BuildType build;
+		build.name = "<unnamed>";
+		build.buildId = 0;
+		build.fileHash = 0;
+		curBuild = new BuildType(build);
+		log("unnamed init");
+	}*/
+	if (curBuild.buildId > 0)
+	{
+
+		char* pValue;
+		size_t len;
+		char ladBuff[512];
+		errno_t err = _dupenv_s(&pValue, &len, "LOCALAPPDATA");
+		strncpy_s(ladBuff, 256, pValue, len);
+		strncpy_s(ladBuff + len - 1, 256 - len, "\\Chivalry 2\\Saved\\Config\\c2uc.builds.json", 42);
+
+		/*for (uint8_t i = 0; i < F_MaxFuncType; ++i)
+			curBuild.offsets[i] = addrOffsets[i];*/
+
+		log(ladBuff);
+		std::ofstream out(ladBuff);
+		/*out << "{\n\"" << curBuildInfo->buildStr << "\": {";
+		out << "\n\"Build\" : " << curBuildInfo->buildId;
+		out << ",\n\"FileHash\" : " << calculateCRC32("Chivalry2-Win64-Shipping.exe");
+		for (uint8_t i = 0; i < F_MaxFuncType; ++i)
+			out << ",\n\"" << strFunc[i] << "\": " << addrOffsets[i];*/
+
+		out << "\n{\n\"" << curBuild.name << "\": {";
+		out << "\n\"Build\" : " << curBuild.buildId;
+		out << ",\n\"FileHash\" : " << curBuild.fileHash;
+		for (uint8_t i = 0; i < F_MaxFuncType; ++i)
+			out << ",\n\"" << strFunc[i] << "\": " << curBuild.offsets[i];
+
+		for (auto build : configBuilds)
+		{
+			out << "\n},\n\"" << build->name << "\": {";
+			out << "\n\"Build\" : " << build->buildId;
+			out << ",\n\"FileHash\" : " << build->fileHash;
+			for (uint8_t i = 0; i < F_MaxFuncType; ++i)
+				out << ",\n\"" << strFunc[i] << "\": " << build->offsets[i];
+		}
+		out << "\n}";
+		out << "\n}";
+
+	}
+
+}
+
+DECL_HOOK(FString*, FViewport, (FViewport_C* this_ptr, void* viewportClient))
+{
+	auto val = o_FViewport(this_ptr, viewportClient);
+	wchar_t* buildNr = wcschr(this_ptr->AppVersionString.str, L'+') + 1;
+	if (buildNr != nullptr)
+	{
+		uint32_t buildId = _wtoi(buildNr);
+		if (curBuild.buildId == 0)
+		{
+			needsSerialization = true;
+			auto bi = new BuildInfo(this_ptr->AppVersionString.str + 7, buildId); // FIXME
+
+			curBuild.name = bi->buildStr;
+			curBuild.buildId = buildId;
+			curBuild.fileHash = calculateCRC32("Chivalry2-Win64-Shipping.exe");
+			/*for (uint8_t i = 0; i < F_MaxFuncType; ++i)
+				curBuild.offsets[i] = addrOffsets[i];*/
+			//curBuild = new BuildType(build);
+			log("Viewport constructor!");
+			if (offsetsLoaded)
+				serializeBuilds();
+		}
+		/*else if (curBuild && curBuild->buildId == 0)
+		{ }*/
+		else
+			log("Viewport constructor! (loaded)");
+			
+			
+
+#ifdef _DEBUG
+		std::wcout << this_ptr->AppVersionString.str << ": " << buildNr << " " << buildId << std::endl;
+#endif
+	}
+	return val;
+}
+
+int LoadBuildConfig()
+{
+	// load config file
+	char* pValue;
+	size_t len;
+	char ladBuff[256];
+	errno_t err = _dupenv_s(&pValue, &len, "LOCALAPPDATA");
+	strncpy_s(ladBuff, 256, pValue, len);
+	strncpy_s(ladBuff + len - 1, 256 - len, "\\Chivalry 2\\Saved\\Config\\c2uc.builds.json", 42);
+
+	std::ifstream file(ladBuff, std::ios::binary);
+	if (!file.is_open()) {
+		std::cout << "Error opening build config" << std::endl;
+		return 0;
+	}
+	std::string buffer;
+	file.seekg(0, std::ios::end);
+	buffer.reserve(file.tellg());
+	file.seekg(0, std::ios::beg);
+
+	buffer.assign((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	file.close();
+
+	// parse json
+	json_t mem[2048];
+	const json_t* json = json_create(const_cast<char*>(buffer.c_str()), mem, 2048);
+
+	if (!json) {
+		puts("Failed to create json parser");
+		return EXIT_FAILURE;
+	}
+	uint32_t curFileHash = calculateCRC32("Chivalry2-Win64-Shipping.exe");
+
+	json_t const* buildEntry;
+	needsSerialization = true;
+	for (buildEntry = json_getChild(json); buildEntry != 0; buildEntry = json_getSibling(buildEntry)) {
+		if (JSON_OBJ == json_getType(buildEntry)) {
+
+			char const* fileSize = json_getPropertyValue(buildEntry, "FileSize");
+			json_t const* build = json_getProperty(buildEntry, "Build");
+			char const* buildName = json_getName(buildEntry);
+			printf("parsing %s\n", buildName);
+
+			json_t const* fileHash = json_getProperty(buildEntry, "FileHash");
+			if (!fileHash || JSON_INTEGER != json_getType(fileHash)) {
+				puts("Error, the fileHash property is not found.");
+				return EXIT_FAILURE;
+			}
+
+			uint64_t fileHashVal = json_getInteger(fileHash);
+			bool hashMatch = fileHashVal == curFileHash;
+
+			if (hashMatch)
+			{
+				needsSerialization = false;
+				printf("Hash match (0x%llx) Build: %s\n", fileHashVal, buildName);
+			}
+
+			BuildType bd;
+			bd.name = std::string(buildName);
+			bd.buildId = (uint32_t)json_getInteger(build);
+			bd.fileHash = (uint32_t)fileHashVal;
+			for (uint8_t i = 0; i < F_MaxFuncType; ++i)
+			{
+				if (const json_t* GetMotd_j = json_getProperty(buildEntry, strFunc[i]))
+				{
+					if (JSON_INTEGER == json_getType(GetMotd_j))
+						if (uint32_t offsetVal = (uint32_t)json_getInteger(GetMotd_j))
+							bd.offsets[i] = offsetVal;
+				}
+				else if (hashMatch) needsSerialization = true;
+			}
+
+			if (hashMatch)
+				curBuild = bd;
+			else
+				configBuilds.push_back(new BuildType(bd));
+		}
+	}
+}
+
+
+
 unsigned long main_thread(void* lpParameter) {
-	log("BrowserPlugin started!");
+	log(logo);
+	log("Chivalry 2 Unchained");
 	MH_Initialize();
+	// https://github.com/HoShiMin/Sig
+	const void* found = nullptr;
+	//found = Sig::find(buf, size, "11 22 ? 44 ?? 66 AA bb cC Dd");
+	//HMODULE var = GetModuleHandle(NULL);
+	//GetModuleInformation(GetCurrentProcess(), var, &moduleInfo, sizeof(moduleInfo));
+	LoadBuildConfig();
+	baseAddr = GetModuleHandleA("Chivalry2-Win64-Shipping.exe");
+	//cerTest();
+	//std::cout << std::hex << calculateCRC32("Chivalry2-Win64-Shipping.exe") << std::endl;
+	int file_descript;
+	//unsigned long file_size;
+	errno_t err = _sopen_s(&file_descript, "Chivalry2-Win64-Shipping.exe", O_RDONLY, _SH_DENYNO, 0);
+	if (err)
+		std::cout << "error " << err << std::endl;
 
-	unsigned char* module_base{ reinterpret_cast<unsigned char*>(GetModuleHandleA("Chivalry2-Win64-Shipping.exe")) };
+	// Get the size of the file
+	off_t file_size = _filelength(file_descript);
 
-	MH_CreateHook(module_base + 0x13da7d0, hk_GetMotd, reinterpret_cast<void**>(&o_GetMotd));
-	MH_EnableHook(module_base + 0x13da7d0);
+	//file_size = get_size_by_fd(file_descript);
+	/*printf("file size:\t%lu\n", file_size);*/
 
-	MH_CreateHook(module_base + 0x13DA280, hk_GetCurrentGames, reinterpret_cast<void**>(&o_GetCurrentGames));
-	MH_EnableHook(module_base + 0x13DA280);
+	//MODULEINFO moduleInfo;	
+	GetModuleInformation(GetCurrentProcess(), baseAddr, &moduleInfo, sizeof(moduleInfo));
 
-	MH_CreateHook(module_base + 0x14a1250, hk_SendRequest, reinterpret_cast<void**>(&o_SendRequest));
-	MH_EnableHook(module_base + 0x14a1250);
+	//std::cout << "MINFO: " << moduleInfo.SizeOfImage << " " << moduleInfo.EntryPoint << std::endl;
+
+	//uint32_t crc = crc32c_append(0, (uint8_t *)baseAddr, moduleInfo.SizeOfImage);
+
+	unsigned char* module_base{ reinterpret_cast<unsigned char*>(baseAddr) };
+
+	for (uint8_t i = 0; i < F_MaxFuncType; ++i)
+	{
+		if (curBuild.offsets[i] == 0)
+			curBuild.offsets[i] = FindSignature(baseAddr, moduleInfo.SizeOfImage, strFunc[i], signatures[i]);
+		else printf("ok -> %s : (conf)\n", strFunc[i]);
+		if (i == F_FViewport)
+		{
+			HOOK_ATTACH(module_base, FViewport);
+		}
+	}
+
+
+
+	char buff[512];
+	char* dest = buff;
+
+	offsetsLoaded = true;
+	serializeBuilds();
+	// official
+	//auto sig_SendRequest= 0x14a1250;
+	//auto sig_GetMotd = 0x13da7d0;
+	//auto sig_GetCurrentGames = 0x13da280;
+	//auto sig_IsNonPakFilenameAllowed = 0x2fc3ce0;
+	//auto sig_FindFileInPakFiles_1 = 0x2fbf1a0;
+	//auto sig_FindFileInPakFiles_2 = 0x2fbf280;
+	//auto sig_UTBLLocalPlayer = 0x199cda3;
+
+	// ptr
+	//auto sig_SendRequest = 0x1425a10;
+	//auto sig_GetMotd = 0x135eb70;
+	//auto sig_GetCurrentGames = 0x135e620;
+	//auto sig_IsNonPakFilenameAllowed = 0x2f4dd80;
+	//auto sig_FindFileInPakFiles_1 = 0x2f49240;
+	//auto sig_FindFileInPakFiles_2 = 0x2f49320;
+	//auto sig_UTBLLocalPlayer = 0x1924926;
+
+	//HOOK_ATTACH(module_base, FViewport);
+	HOOK_ATTACH(module_base, GetMotd);
+	HOOK_ATTACH(module_base, GetCurrentGames);
+	HOOK_ATTACH(module_base, SendRequest);
+	HOOK_ATTACH(module_base, IsNonPakFilenameAllowed);
+	HOOK_ATTACH(module_base, FindFileInPakFiles_1);
+	HOOK_ATTACH(module_base, FindFileInPakFiles_2);
+	HOOK_ATTACH(module_base, GetGameInfo);
+
+
+	// ServerPlugin
+	auto cmd_permission{ module_base + curBuild.offsets[F_UTBLLocalPlayer_Exec] }; // Patch for command permission when executing commands (UTBLLocalPlayer::Exec)
+
+	// 75 1A 45 84 ED 75 15 48 85 F6 74 10 40 38 BE ? ? ? ? 74 07 32 DB E9 ? ? ? ? 48 8B 5D 60 49 8B D6 4C 8B 45 58 4C 8B CB 49 8B CF (Points directly to instruction: first JNZ)
+
+	DWORD d;
+	VirtualProtect(cmd_permission, 1, PAGE_EXECUTE_READWRITE, &d);
+	*cmd_permission = 0xEB; // Patch to JMP
+	VirtualProtect(cmd_permission, 1, d, NULL); //TODO: Convert patch to hook.
 
 	ExitThread(0);
 	return 0;
